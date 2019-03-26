cdd83c0081f2d1fd8c1b820f30916649e062426d
MessyShen <iamsherlocked@outlook.com>
2019-03-26 15:01:06 +0800

    add slice list info

--- FaceAlignment/Experiment/convertBndToXYZ.py
+++ FaceAlignment/Experiment/convertBndToXYZ.py
@@ -1,4 +1,32 @@
 # coding = utf-8
+import os
+
 import numpy as np
 
 
+def convertBND2XYZ(bndFile, writePath):
+    bnd = np.loadtxt(bndFile)
+    savePath = bndFile[:-4] + "Export.xyz"
+    np.savetxt(savePath, bnd, fmt='%.7f')
+    '''
+    # TODO: find the mapping relationship
+    RE  = right eye,
+    LE  = left  eye,
+    REB = right eye brow,
+    LEB = left  eye brow,
+    NS  = Nose,
+    OUL = Outer Upper Lips
+    IUL = Inner Upper Lips
+    
+    '''
+    SliceList = [0,   8,   16,    26,    36,   48,    55,    60,    65,    68,   83]
+    Name      = [  'RE', 'LE', 'REB', 'LEB', 'NS', 'OUL', 'OLL', 'IUL', 'ILL', 'OC'] 
+    for i in range(len(Name)):
+        savePath = bndFile[:-4] + Name[i] + ".xyz"
+        np.savetxt(savePath, bnd[SliceList[i]:SliceList[i+1], :], fmt="%.7f") 
+
+
+if __name__ == '__main__':
+    convertBND2XYZ('FaceAlignment/Experiment/noseA.bnd', 'FaceAlignment/Experiment/')
+    # a = np.loadtxt('FaceAlignment/Experiment/noseB.bnd')
+    # np.savetxt('FaceAlignment/Experiment/noseB_.bnd', a[:,1:], fmt='%.7f')
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # 3DFace2.0
+
+gitignored file: /BU3D/, /tempData/
+
+'''
+├───BU3D
+│   ├───landmarks
+│   ├───train
+│   ├───train_Resampled
+│   ├───val
+│   └───val_Resampled
+├───combineMeshAndPoint
+│   └───__pycache__
+├───dataGenerator
+├───DicLearning
+│   └───__pycache__
+├───FaceAlignment
+│   ├───Experiment
+│   ├───ExportBU3D
+│   │   ├───landmarks
+│   │   └───train
+│   └───__pycache__
+├───Resampling
+│   ├───file
+│   └───Resampled
+│       └───file
+└───tempData
+'''
+
